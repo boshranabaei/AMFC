85b9cb8e97e08661e3c45e62222959d53f497dcc
Boshra Nabaei <b.nabaei@gmail.com>
2016-08-14 23:07:27 -0700

    Adding form for add applicants

--- WebContent/amfc/AApp.view.js
+++ WebContent/amfc/AApp.view.js
@@ -20,89 +20,52 @@ sap.ui.jsview("amfc.AApp", {
 	 */
 	createContent : function(oController) {
 
-		var aData = [
-		             {select:false,firstName:"Hamed", lastName:"aaaa", dateApplied:"2016-05-03",viewProfile:"View Profile"},
-		             {select:false,firstName:"Ahmad",lastName:"bbbb", dateApplied:"2016-07-19",viewProfile:"View Profile"},
-		             {select:false,firstName:"Amal",lastName:"cccc", dateApplied:"2016-08-30",viewProfile:"View Profile"}
-		             ];
+		var oSimpleForm = new sap.ui.layout.form.SimpleForm(
+				"sf1",
+				{
+					layout: sap.ui.layout.form.SimpleFormLayout.GridLayout,
+					editable: true,
+					content:[
+					        new sap.ui.core.Title({text:"Personal Data"}),
+							new sap.m.Label({text:"First Name"}),
+							new sap.m.Input({}),
+							new sap.m.Label({text:"Last Name"}),
+							new sap.m.Input({}),
+							new sap.m.Label({text:"Date of birth"}),
+							new sap.m.DatePicker({yyyymmdd:"19900101"}),
+							new sap.m.Label({text:"Gender"}),
+							new sap.m.RadioButtonGroup({
+								columns: 2,
+								items: [new sap.ui.core.Item({text: "male"}),
+												new sap.ui.core.Item({text: "female"})]
+							}),
+							new sap.m.Label({text:"Marital Status"}),
+							new sap.ui.commons.DropdownBox("maritalStatus",{
+								items:[new sap.ui.core.ListItem({id:"single", text:"single"}),
+						                new sap.ui.core.ListItem({id:"divorced", text:"divorced"}),
+						                new sap.ui.core.ListItem({id:"widowed", text:"widowed"}),
+										new sap.ui.core.ListItem({id:"separated", text:"separated"})
+						            ],
+								selectedItemId: "single",
+								change: function(){
+						                var x = this.getSelectedItemId();
+						            }
+							}),
+							new sap.m.Label({text:"Number of children if not single"}),
+							new sap.m.Input({id:"children", value:0}),
+							new sap.ui.core.Title({text:"Personal Data"}),
+							new sap.m.Label({text:"ethnicity"}),
+							new sap.m.Input({}),
+							new sap.ui.core.Title({text:"Personal Data"}),
+							new sap.m.Label({text:"ethnicity"}),
+							new sap.m.Input({}),
+							new sap.ui.core.Title({text:"Personal Data"}),
+							new sap.m.Label({text:"ethnicity"}),
+							new sap.m.Input({})
+							]
+				});
 		
-		var applyLabel = new sap.m.Label(this.createId("applyLabel"), {
-			text : "Apply changes to the selected applicants:"
-		});
-
-		var oTable = new sap.ui.table.Table({
-			visibleRowCount: 3,
-			selectionMode: sap.ui.table.SelectionMode.None,
-			fixedColumnCount: 5,
-			width: "540px"
-		});
-		
-		
-		oTable.addColumn(new sap.ui.table.Column({
-			label: new sap.ui.commons.Label({text: "Select"}),
-			template: new sap.ui.commons.CheckBox().bindProperty("checked", "select"),
-			width: "60px"
-		}));
-		
-		oTable.addColumn(new sap.ui.table.Column({
-			label: new sap.ui.commons.Label({text: "First Name"}),
-			template: new sap.ui.commons.TextView().bindProperty("text", "firstName"),
-			sortProperty: "firstName",
-			filterProperty: "firstName",
-			width: "120px"
-		}));
-        
-		oTable.addColumn(new sap.ui.table.Column({
-			label: new sap.ui.commons.Label({text: "Last Name"}),
-			template: new sap.ui.commons.TextView().bindProperty("text", "lastName"),
-			sortProperty: "lastName",
-			filterProperty: "lastName",
-			width: "120px"
-		}));
-		
-		oTable.addColumn(new sap.ui.table.Column({
-			label: new sap.ui.commons.Label({text: "Date Applied"}),
-			template: new sap.ui.commons.TextView().bindProperty("text", "dateApplied"),
-			sortProperty: "lastName",
-			filterProperty: "lastName",
-			width: "120px"
-		}));
-		
-		oTable.addColumn(new sap.ui.table.Column({
-			label: new sap.ui.commons.Label({text: "View Profile"}),
-			template:
-			
-			new sap.m.Button(this.createId("viewProfile"), {
-				text : "View Profile",
-				press : function() {
-				}
-			}).bindProperty("text", "viewProfile"),
-			width: "120px"
-		}));
-		
-        var oModel = new sap.ui.model.json.JSONModel();
-        oModel.setData({modelData: aData});
-        oTable.setModel(oModel);
-        oTable.bindRows("/modelData");
-
-
-        var accept = new sap.m.Button(this.createId("accept"), {
-			text : "Accept",
-			width : "10em",
-			press : function() {
-			}
-		});
-        var reject = new sap.m.Button(this.createId("reject"), {
-			text : "Reject",
-			width : "10em",
-			press : function() {
-			}
-		});
-        
-        var applyVBox = new sap.m.VBox(this.createId("filterVBox")).setAlignItems("Center");
-        applyVBox.addItem(applyLabel).addItem(accept).addItem(reject);
-
-        return new sap.m.HBox(this.createId("containerLayoutHBox")).addItem(applyVBox).addItem(oTable);
+        return new sap.m.HBox(this.createId("formHBox")).addItem(oSimpleForm);
 
 	}
 
