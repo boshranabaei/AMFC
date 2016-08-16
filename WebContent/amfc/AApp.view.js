69dd8fb652ca99a5b65bf022bdcac1ca0260e02a
Boshra Nabaei <b.nabaei@gmail.com>
2016-08-15 20:41:35 -0700

    Editing form

--- WebContent/amfc/AApp.view.js
+++ WebContent/amfc/AApp.view.js
@@ -36,11 +36,11 @@ sap.ui.jsview("amfc.AApp", {
 							new sap.m.Label({text:"Gender"}),
 							new sap.m.RadioButtonGroup({
 								columns: 2,
-								items: [new sap.ui.core.Item({text: "male"}),
-												new sap.ui.core.Item({text: "female"})]
+								buttons: [new  sap.m.RadioButton({text: "male"}),
+										  new  sap.m.RadioButton({text: "female"})]
 							}),
 							new sap.m.Label({text:"Marital Status"}),
-							new sap.ui.commons.DropdownBox("maritalStatus",{
+							new sap.m.ComboBox("maritalStatus",{
 								items:[new sap.ui.core.ListItem({id:"single", text:"single"}),
 						                new sap.ui.core.ListItem({id:"divorced", text:"divorced"}),
 						                new sap.ui.core.ListItem({id:"widowed", text:"widowed"}),
@@ -53,11 +53,40 @@ sap.ui.jsview("amfc.AApp", {
 							}),
 							new sap.m.Label({text:"Number of children if not single"}),
 							new sap.m.Input({id:"children", value:0}),
-							new sap.ui.core.Title({text:"Personal Data"}),
-							new sap.m.Label({text:"ethnicity"}),
+							new sap.m.Label({text:"Ethnicity"}),
+							new sap.m.Input({}),
+							new sap.m.Label({text:"Citizenship"}),
+							new sap.m.Input({}),
+							new sap.m.Label({text:"Highest Level of Education"}),
+							new sap.m.ComboBox("education",{
+								items:[new sap.ui.core.ListItem({id:"PrimarySchool", text:"Primary School"}),
+						                new sap.ui.core.ListItem({id:"SecondarySchool", text:"Secondary School"}),
+						                new sap.ui.core.ListItem({id:"PostSecondarySchool", text:"Post Secondary School"}),
+						                new sap.ui.core.ListItem({id:"Bachelors", text:"Bachelors"}),
+										new sap.ui.core.ListItem({id:"Masters", text:"Masters"}),
+										new sap.ui.core.ListItem({id:"PhD", text:"PhD"})
+						            ],
+								change: function(){
+							                var x = this.getSelectedItemId();
+							            }
+								}),
+							new sap.m.Label({text:"Occupation"}),
+							new sap.m.Input({}),
+							new sap.ui.core.Title({text:"Contact Info"}),
+							new sap.m.Label({text:"Email:"}),
+							new sap.m.Input({}),
+							new sap.m.Label({text:"Phone Number (mobile)"}),
+							new sap.m.Input({}),
+							new sap.m.Label({text:"Phone Number (home)"}),
+							new sap.m.Input({}),
+							new sap.m.Label({text:"City"}),
+							new sap.m.Input({}),
+							new sap.m.Label({text:"Province"}),
+							new sap.m.Input({}),
+							new sap.m.Label({text:"Country"}),
 							new sap.m.Input({}),
 							new sap.ui.core.Title({text:"Personal Data"}),
-							new sap.m.Label({text:"ethnicity"}),
+					        new sap.m.Label({text:"ethnicity"}),
 							new sap.m.Input({}),
 							new sap.ui.core.Title({text:"Personal Data"}),
 							new sap.m.Label({text:"ethnicity"}),
