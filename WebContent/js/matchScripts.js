f6cbdccd56aadbeea308780b9da383f1c6be2e19
Boshra Nabaei <b.nabaei@gmail.com>
2017-02-12 16:09:11 -0800

    Current Applicants Segment created

--- WebContent/js/matchScripts.js
+++ WebContent/js/matchScripts.js
@@ -23,20 +23,71 @@ requestApplicants()
 function drawTable() {
 
 	var table = document.createElement('table');
-	for (var i = 1; i <  APPLICANTS.length; i++){
+	
+	var headers = ["Name", "Age", "Status", "Date Applied", "List","View Profile"];
+	var header = table.createTHead();
+	var row = header.insertRow(0);
+	for(var i = 0; i <  headers.length; i++){
+		var cell = row.insertCell(i);
+		cell.innerHTML = headers[i];
+		cell.style.background = "#5a8c6d";
+		cell.style.color = "white";
+	}
+	
+	for (var i = 0; i <  APPLICANTS.length; i++){
 	    var tr = document.createElement('tr');   
 	
-	    var td1 = document.createElement('td');
-	    var td2 = document.createElement('td');
-	
-	    var text1 = document.createTextNode('Text1');
-	    var text2 = document.createTextNode('Text2');
-	
-	    td1.appendChild(text1);
-	    td2.appendChild(text2);
-	    tr.appendChild(td1);
-	    tr.appendChild(td2);
-	
+	    var name = document.createElement('td');
+	    name.appendChild(document.createTextNode(APPLICANTS[i].firstName +" "+APPLICANTS[i].lastName));
+	    tr.appendChild(name);
+	    name.style.fontWeight="bold";
+	    name.style.width= "12em";
+	    
+	    var age = document.createElement('td');
+	    age.appendChild( document.createTextNode(APPLICANTS[i].age) );
+	    tr.appendChild(age);
+	    
+	    var status = document.createElement('td');
+	    status.appendChild( document.createTextNode(APPLICANTS[i].status) );
+	    tr.appendChild(status);
+	    if(APPLICANTS[i].status=="free")
+	    	status.style.color="grey";
+	    else if(APPLICANTS[i].status=="busy")
+	    	status.style.color="red";
+	    status.style.width="5em";
+	    
+	    var dateApplied = document.createElement('td');
+	    dateApplied.appendChild( document.createTextNode(APPLICANTS[i].dateAdded) );
+	    tr.appendChild(dateApplied);
+	    dateApplied.style.width="7em";
+	    
+	    var list = document.createElement('td');
+	    var editBtn = document.createElement("BUTTON");
+	    var editText = document.createTextNode("MATCH");
+	    editBtn.appendChild( editText );
+	    list.appendChild( document.createTextNode(0));
+	    list.appendChild( editBtn );
+	    tr.appendChild(list);
+	    list.style.width="5em";
+	    
+	    var profile = document.createElement('td');
+	    var profilelogo = document.createElement("img");
+	    profile.appendChild( profilelogo );
+	    tr.appendChild(profile);
+	    profile.style.width="8em";
+	    profilelogo.src="/../img/profile.png";
+	    profilelogo.style.height="2em";
+	    profilelogo.style.marginTop="0.2em";
+	    profilelogo.style.boxShadow= "2px 2px #999";
+	    profilelogo.style.boxShadow= "2px 2px #999";
+	    
+	    //http://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
+	    
+	    if(i%2==0)
+	    	tr.style.background="white";
+	    else
+	    	tr.style.background="#e0ebe4";
+	    	
 	    table.appendChild(tr);
 	}
 	document.body.appendChild(table);
--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -26,41 +26,12 @@ var addEvent = (function () {
 // variables
 var aLoops = []; // sound loops
 
-// initialization
-addEvent(window, 'load', function (event) {
-
-    // load music files
-    aLoops[0] = new Audio('media/background.ogg');
-    aLoops[0].volume = 0.3;
-    aLoops[1] = new Audio('media/button.ogg');
-    aLoops[1].volume = 1.0;
-    aLoops[2] = new Audio('media/button_click.ogg');
-    aLoops[2].volume = 1.0;
-
-    aLoops[0].addEventListener('ended', function() { // loop background sound
-        this.currentTime = 0;
-        this.play();
-    }, false);
-    aLoops[0].play();
-});
-
 // all the buttons
 var aBtns = document.querySelectorAll('#nav li');
 
-// onmouseover event handler
-addEvent(aBtns, 'mouseover', function (event) {
-    aLoops[1].currentTime = 0;
-    aLoops[1].play(); // play click sound
-});
+function logOut() {
+	
+	window.open("index.html","_self");
+	
+};
 
-// onmouseout event handler
-addEvent(aBtns, 'mouseout', function (event) {
-    aLoops[1].currentTime = 0;
-    aLoops[1].pause(); // play click sound
-});
-
-// onclick event handler
-addEvent(aBtns, 'click', function (event) {
-    aLoops[2].currentTime = 0;
-    aLoops[2].play(); // play click sound
-});
